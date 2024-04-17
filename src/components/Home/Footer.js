21050706d3f5efeceb5af2c70ab5504adf5b5a1f
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-04-17 20:43:36 +0700

    ridwan

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,54 +1,89 @@
-import React from 'react';
+import React from "react";
 import footer from "../../../public/assets/footer.png";
 import logo from "../../../public/assets/LOGO BATUGIN-01.png";
-import Image from 'next/image';
+import Image from "next/image";
 
 function FooterComp() {
-    return (
-        <div className='mt-[20rem] flex justify-center items-center '>
-            <div className='h-[19rem] w-full bg-no-repeat bg-cover bg-center ' style={{ backgroundImage: `url('${footer.src}')` }}>
-                <div className='flex justify-center items-center h-full'>
-                    <div className='grid grid-cols-5 gap-4 w-full justify-items-center'> 
-                        <div className='flex justify-center'> 
-                            <Image src={logo} />
-                        </div>
-                        <div className='flex flex-col text-start'>
-                            <div className='font-semibold text-accents-2 text-[1.25rem]'>Tentang</div>
-                            <div className='text-[1rem] text-accents-2 '>
-                                <div>About</div>
-                                <div>Help Center</div>
-                                <div>Store</div>
-                            </div>
-                        </div>
-                        <div className='flex flex-col text-start'>
-                            <div className='font-semibold text-accents-2 text-[1.25rem]'>Social Media</div>
-                            <div className='text-[1rem] text-accents-2 '>
-                                <div>Instagram</div>
-                                <div>Facebook</div>
-                                <div>Twitter</div>
-                            </div>
-                        </div>
-                        <div className='flex flex-col text-start'>
-                            <div className='font-semibold text-accents-2 text-[1.25rem]'>Social Media</div>
-                            <div className='text-[1rem] text-accents-2 '>
-                                <div>Instagram</div>
-                                <div>Facebook</div>
-                                <div>Twitter</div>
-                            </div>
-                        </div>
-                        <div className='flex flex-col text-start'>
-                            <div className='font-semibold text-accents-2 text-[1.25rem]'>Social Media</div>
-                            <div className='text-[1rem] text-accents-2 '>
-                                <div>Instagram</div>
-                                <div>Facebook</div>
-                                <div>Twitter</div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <div className="mt-[20rem] md:flex justify-center items-center   hidden">
+        <div
+          className="h-[19rem] w-full bg-no-repeat bg-cover bg-center "
+          style={{ backgroundImage: `url('${footer.src}')` }}
+        >
+          <div className="flex justify-center items-center h-full">
+            <div className="grid grid-cols-5 gap-4 w-full justify-items-center">
+              <div className="flex justify-center">
+                <Image src={logo} />
+              </div>
+              <div className="flex flex-col text-start">
+                <div className="font-semibold text-accents-2 text-[1.25rem]">
+                  Tentang
                 </div>
+                <div className="text-[1rem] text-accents-2 ">
+                  <div>About</div>
+                  <div>Help Center</div>
+                  <div>Store</div>
+                </div>
+              </div>
+              <div className="flex flex-col text-start">
+                <div className="font-semibold text-accents-2 text-[1.25rem]">
+                  Social Media
+                </div>
+                <div className="text-[1rem] text-accents-2 ">
+                  <div>Instagram</div>
+                  <div>Facebook</div>
+                  <div>Twitter</div>
+                </div>
+              </div>
+              <div className="flex flex-col text-start">
+                <div className="font-semibold text-accents-2 text-[1.25rem]">
+                  Social Media
+                </div>
+                <div className="text-[1rem] text-accents-2 ">
+                  <div>Instagram</div>
+                  <div>Facebook</div>
+                  <div>Twitter</div>
+                </div>
+              </div>
+              <div className="flex flex-col text-start">
+                <div className="font-semibold text-accents-2 text-[1.25rem]">
+                  Social Media
+                </div>
+                <div className="text-[1rem] text-accents-2 ">
+                  <div>Instagram</div>
+                  <div>Facebook</div>
+                  <div>Twitter</div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+      <div
+        className="w-full  flex mt-[20rem] h-screen  md:hidden"
+        style={{
+          backgroundImage: "url('/assets/ft-bawah-hp.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className=" w-full mt-[3.7rem] pr-[1.4rem] pl-[1.4rem]">
+          <Image src={logo} />
+          <div className="flex flex-col text-start">
+            <div className="font-semibold text-accents-2 text-[1.45rem]">
+              Tentang
+            </div>
+            <div className="text-[0.85rem] text-accents-2 ">
+              <div>About</div>
+              <div>Help Center</div>
+              <div>Store</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FooterComp;
