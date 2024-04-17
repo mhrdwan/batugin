64300fb515ee039ccc81f17a8893e34f46124cf6
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-04-17 21:15:07 +0700

    ridwan

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -61,7 +61,7 @@ function FooterComp() {
         </div>
       </div>
       <div
-        className="w-full  flex mt-[20rem] h-screen  md:hidden"
+        className="w-full  flex mt-[20rem] h-[50rem]  md:hidden"
         style={{
           backgroundImage: "url('/assets/ft-bawah-hp.png')",
           backgroundRepeat: "no-repeat",
@@ -78,6 +78,30 @@ function FooterComp() {
               <div>About</div>
               <div>Help Center</div>
               <div>Store</div>
+            </div>
+            <div className="font-semibold mt-[3.7rem]  text-accents-2 text-[1.45rem]">
+            Social Media
+            </div>
+            <div className="text-[0.85rem] text-accents-2 ">
+              <div>Instagram</div>
+              <div>Facebook</div>
+              <div>Twitter</div>
+            </div>
+            <div className="font-semibold mt-[3.7rem]  text-accents-2 text-[1.45rem]">
+            Others
+            </div>
+            <div className="text-[0.85rem] text-accents-2 ">
+              <div>Store</div>
+              <div>Points</div>
+              <div>Laddeboard</div>
+            </div>
+            <div className="font-semibold mt-[3.7rem]  text-accents-2 text-[1.45rem]">
+            Contact
+            </div>
+            <div className="text-[0.85rem] text-accents-2 ">
+              <div>batugin.support@gmail.com</div>
+              <div>(021) 1238-2309</div>
+              <div>(Whatsapp) 0812-2389-1239 </div>
             </div>
           </div>
         </div>
