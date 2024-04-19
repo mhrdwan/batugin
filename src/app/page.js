a1d0bd2a40f7f1b2ca24ed7d7d7f453323fd2090
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-04-19 21:23:51 +0700

    memek

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import ApaItu from "@/components/Home/ApaItu";
 import ArtikelKami from "@/components/Home/ArtikelKami";
 import FollowBatugin from "@/components/Home/Follow";
@@ -7,7 +7,8 @@ import Slide1 from "@/components/Home/Slide1";
 import NavbarComp from "@/components/NavbarComp";
 import NorifReward from "@/components/NorifReward";
 import Image from "next/image";
-import up from "../../public/assets/arrow-up-circle.png"
+import up from "../../public/assets/arrow-up-circle.png";
+import BATUGIN_PINTU from "../../public/assets/BATUGIN_PINTU.png";
 import { useEffect, useRef, useState } from "react";
 // export const metadata = {
 //   title: "Batugin",
@@ -23,28 +24,29 @@ export default function Home() {
     // Opsional: Kondisi tertentu untuk melakukan scroll, misalnya berdasarkan state atau props
     // Jika kondisi terpenuhi, lakukan scroll ke komponen
     if (apaItuRef.current) {
-      apaItuRef.current.scrollIntoView({ behavior: 'smooth' });
+      apaItuRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, []); // Kosongkan dependency array jika Anda ingin scroll terjadi saat komponen dimuat
 
   useEffect(() => {
     const checkScrollPosition = () => {
-      if (!showScrollButton && window.pageYOffset > 500) { // Misal, tombol muncul ketika user scroll lebih dari 500px
+      if (!showScrollButton && window.pageYOffset > 500) {
+        // Misal, tombol muncul ketika user scroll lebih dari 500px
         setShowScrollButton(true);
       } else if (showScrollButton && window.pageYOffset <= 500) {
         setShowScrollButton(false);
       }
     };
 
-    window.addEventListener('scroll', checkScrollPosition);
+    window.addEventListener("scroll", checkScrollPosition);
 
-    return () => window.removeEventListener('scroll', checkScrollPosition);
+    return () => window.removeEventListener("scroll", checkScrollPosition);
   }, [showScrollButton]);
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // Untuk scroll secara smooth
+      behavior: "smooth", // Untuk scroll secara smooth
     });
   };
 
@@ -57,12 +59,30 @@ export default function Home() {
       <Slide1 />
 
       <ApaItu />
+      <div className="pl-[1.4rem] pr-[1.4rem] flex justify-center w-full mt-[5rem]">
+        <Image
+          src={BATUGIN_PINTU}
+          alt="BATUGIN_PINTU"
+          width={670}
+          height={100}
+        />
+      </div>
       <ArtikelKami />
       <FollowBatugin />
-      <FooterComp mt={20}/>
+      <FooterComp mt={20} />
 
       {showScrollButton && (
-        <Image onClick={scrollToTop} src={up} style={{ width: "3rem", position: 'fixed', bottom: '20px', right: '20px' }} className="hover:cursor-pointer" />
+        <Image
+          onClick={scrollToTop}
+          src={up}
+          style={{
+            width: "3rem",
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+          }}
+          className="hover:cursor-pointer"
+        />
         // <button onClick={scrollToTop} style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
         //  Balik Ke Atas
         // </button>
