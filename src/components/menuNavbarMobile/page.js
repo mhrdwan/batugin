b83765d379b946c5442f98cb5941ebf6400c3adb
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-04-20 12:32:12 +0700

    navbar

--- src/components/menuNavbarMobile/page.js
+++ src/components/menuNavbarMobile/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 
 export default function MenuNavbar() {
@@ -7,25 +7,19 @@ export default function MenuNavbar() {
       <div className="w-36 h-52 p-5 bg-neutral-50 rounded-bl-2xl shadow flex flex-col justify-start items-end gap-2.5">
         <div className="w-9 h-9 p-2 flex flex-col justify-center items-center gap-2" />
         <div className="self-stretch p-2.5 flex justify-center items-center gap-2">
-          <Link href={"/tentang-batugin"}>
-            <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
-              About
-            </div>
-          </Link>
+          <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
+            <Link href={"/tentang-batugin"}>About</Link>
+          </div>
         </div>
         <div className="self-stretch p-2.5 flex justify-center items-center gap-2">
-          <Link href={"/"}>
-            <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
-              Product
-            </div>
-          </Link>
+          <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
+            <Link href={"/obat-peluruh-batu-ginjal-pertama-di-indonesia"}>Product</Link>
+          </div>
         </div>
         <div className="w-24 p-2.5 flex justify-center items-center gap-2">
-          <Link href={"/points"}>
-            <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
-              Points
-            </div>
-          </Link>
+          <div className="text-stone-900 text-xs font-medium font-['Roboto'] tracking-tight">
+            <Link href={"/points"}>Points</Link>
+          </div>
         </div>
       </div>
     </div>
