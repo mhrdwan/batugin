42964ae9fed72c313b7249627a1b1565c871dc18
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-06-02 20:03:27 +0700

    ridwann

---
+++ src/app/login/admin/article/page.js
@@ -0,0 +1,73 @@
+"use client";
+import { ArticleZustand } from "@/zustand/Article/article";
+import { Button, Table } from "antd";
+import React, { useEffect } from "react";
+import Sidebar from "../page";
+
+function Article() {
+  const { getArticle, DataArticle, loading } = ArticleZustand();
+  useEffect(() => {
+    getArticle();
+  }, []);
+  const columns = [
+    {
+      title: "No",
+      dataIndex: "no",
+      key: "name",
+      width: 70,
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "age",
+    },
+    {
+      title: "Sub Title",
+      dataIndex: "sub_title",
+      key: "address",
+    },
+    {
+      title: "Foto",
+      dataIndex: "foto",
+      key: "address",
+    },
+    {
+      title: "Edit",
+      render: () => {
+        return (
+          <>
+            <Button
+              size="sm"
+              style={{ backgroundColor: "#1677ff", color: "white" }}
+            >
+              Edit
+            </Button>
+            <Button
+              size="sm"
+              style={{ backgroundColor: "red", color: "white" }}
+            >
+              Hapus
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <Sidebar title="Article">
+        <Table
+          loading={loading}
+          scroll={{
+            y: 240,
+          }}
+          dataSource={DataArticle?.data}
+          columns={columns}
+        />
+      </Sidebar>
+    </div>
+  );
+}
+
+export default Article;
