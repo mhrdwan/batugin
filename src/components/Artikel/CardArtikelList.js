beb44f846a2eb602ac3f98423642b12c132e5b10
root <Ridwan174!>
2024-07-10 14:35:19 +0700

    ridwan

--- src/components/Artikel/CardArtikelList.js
+++ src/components/Artikel/CardArtikelList.js
@@ -18,7 +18,7 @@ export default function CardArtikelList({
   return (
     <div
       onClick={onClick}
-      className="flex bg-white rounded-lg overflow-hidden mb-4 w-full md:w-[30vw] min-h-[150px] hover:cursor-pointer"
+      className="flex bg-white rounded-lg overflow-hidden mb-4 w-full md:w-[35vw] min-h-[150px] hover:cursor-pointer"
     >
       {imageUrl && (
         <div className="w-1/2 h-48 overflow-hidden group">
@@ -29,11 +29,11 @@ export default function CardArtikelList({
           />
         </div>
       )}
-      <div className="w-2/3 p-4 flex flex-col justify-between">
-        <div className="font-bold text-md mb-2">{title}</div>
-        <p className="text-gray-700 text-sm flex-grow">
+      <div className="w-2/3 p-4 flex flex-col justify-center">
+        <div className="font-semibold text-md">{title}</div>
+        {/* <p className="text-gray-700 text-sm flex-grow">
           {truncateText(description, 100)} 
-        </p>
+        </p> */}
         <p className="text-gray-500 text-xs">{time}</p>
       </div>
     </div>
