e6780d10d86af5d80af6b052743e4f47da517031
Administrator <gitlab_admin_c8e1a9@example.com>
2024-07-10 20:06:05 +0700

    ridwan

--- src/components/ModalArtikelBaru/ModalArtikelBaru.js
+++ src/components/ModalArtikelBaru/ModalArtikelBaru.js
@@ -7,95 +7,102 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { ArticleZustand } from "@/zustand/Article/article";
 
 export default function ModalArtikelBaru({ openModal, setOpenModal }) {
-  const [content, setContent] = useState("");
-  const [contentUpload, setContentUpload] = useState({
-    title: "",
-    subTitle: "",
-    image: null, 
-  });
-  const { uploadArtikel } = ArticleZustand();
+	const [content, setContent] = useState("");
+	const [contentUpload, setContentUpload] = useState({
+		title: "",
+		subTitle: "",
+		image: null,
+	});
+	const { uploadArtikel } = ArticleZustand();
 
-  const handleTyping = (e) => {
-    const { name, value } = e.target;
-    setContentUpload({
-      ...contentUpload,
-      [name]: value,
-    });
-  };
+	const handleTyping = (e) => {
+		const { name, value } = e.target;
+		setContentUpload({
+			...contentUpload,
+			[name]: value,
+		});
+	};
 
-  const handleSave = () => {
-    console.log("Artikel disimpan:", { ...contentUpload, content });
-    setOpenModal(false);
-    uploadArtikel({ ...contentUpload, content });
-  };
+	const handleSave = () => {
+		console.log("Artikel disimpan:", { ...contentUpload, content });
+		setOpenModal(false);
+		uploadArtikel({ ...contentUpload, content });
+	};
 
-  const handleUploadChange = (info) => {
-    console.log(`info`, info.file.originFileObj);
-      setContentUpload({
-        ...contentUpload,
-        image: info.file.originFileObj,
-      });
-  };
+	const handleUploadChange = (info) => {
+		console.log(`info`, info.file.originFileObj);
+		setContentUpload({
+			...contentUpload,
+			image: info.file.originFileObj,
+		});
+	};
 
-  return (
-    <Modal
-      title="Artikel Baru"
-      open={openModal}
-      onCancel={() => setOpenModal(false)}
-      footer={null}
-      width={900}
-      style={{ height: 700, display: "flex", flexDirection: "column" }}
-    >
-      {typeof window !== "undefined" && (
-        <>
-          <Row className="flex flex-col space-y-3">
-            <Input
-              name="title"
-              value={contentUpload.title}
-              onChange={handleTyping}
-              placeholder="Masukkan Title"
-            />
-            <Input
-              name="subTitle"
-              value={contentUpload.subTitle}
-              onChange={handleTyping}
-              placeholder="Masukkan Sub Title"
-            />
-            <Upload onChange={handleUploadChange}>
-              <Button>Masukkan Foto</Button>
-            </Upload>
-          </Row>
-          <CKEditor
-            editor={ClassicEditor}
-            data={content}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              setContent(data);
-            }}
-            style={{ height: "400px", marginBottom: "50px" }}
-          />
-          <div
-            style={{
-              marginTop: "auto",
-              display: "flex",
-              justifyContent: "flex-end",
-            }}
-          >
-            <Button
-              onClick={() => setOpenModal(false)}
-              style={{ marginRight: 10 }}
-            >
-              Batal
-            </Button>
-            <Button
-              style={{ backgroundColor: "#fddb1d", color: "black" }}
-              onClick={handleSave}
-            >
-              Simpan
-            </Button>
-          </div>
-        </>
-      )}
-    </Modal>
-  );
+	return (
+		<Modal
+			title="Artikel Baru"
+			open={openModal}
+			onCancel={() => setOpenModal(false)}
+			footer={null}
+			width={900}
+			style={{ height: 700, display: "flex", flexDirection: "column" }}
+		>
+			{typeof window !== "undefined" && (
+				<>
+					<Row className="flex flex-col space-y-3">
+						<Input
+							name="title"
+							value={contentUpload.title}
+							onChange={handleTyping}
+							placeholder="Masukkan Title"
+						/>
+						<Input
+							name="subTitle"
+							value={contentUpload.subTitle}
+							onChange={handleTyping}
+							placeholder="Masukkan Sub Title"
+						/>
+						<Upload onChange={handleUploadChange}>
+							<Button>Masukkan Foto</Button>
+						</Upload>
+					</Row>
+					<CKEditor
+						editor={ClassicEditor}
+						data={content}
+						onChange={(event, editor) => {
+							const data = editor.getData();
+							setContent(data);
+						}}
+						config={{
+							toolbar: [
+								'heading', '|',
+								'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|',
+								'undo', 'redo'
+							]
+						}}
+						style={{ height: "400px", marginBottom: "50px" }}
+					/>
+					<div
+						style={{
+							marginTop: "auto",
+							display: "flex",
+							justifyContent: "flex-end",
+						}}
+					>
+						<Button
+							onClick={() => setOpenModal(false)}
+							style={{ marginRight: 10 }}
+						>
+							Batal
+						</Button>
+						<Button
+							style={{ backgroundColor: "#fddb1d", color: "black" }}
+							onClick={handleSave}
+						>
+							Simpan
+						</Button>
+					</div>
+				</>
+			)}
+		</Modal>
+	);
 }
