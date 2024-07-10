8d38ebee4aca37ccb858128bf42845ecf0d1f029
Administrator <gitlab_admin_c8e1a9@example.com>
2024-07-10 20:25:33 +0700

    ridwan

--- src/components/ModalArtikelBaru/ModalArtikelBaru.js
+++ src/components/ModalArtikelBaru/ModalArtikelBaru.js
@@ -1,108 +1,131 @@
 import React, { useState } from "react";
-import { Modal, Button, Input, Row, Upload } from "antd";
+import { Modal, Button, Input, Row, Upload, message } from "antd";
 import dynamic from "next/dynamic";
 
-const CKEditor = dynamic(() => import("@ckeditor/ckeditor5-react").then(mod => mod.CKEditor), { ssr: false });
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 import { ArticleZustand } from "@/zustand/Article/article";
 
 export default function ModalArtikelBaru({ openModal, setOpenModal }) {
-	const [content, setContent] = useState("");
-	const [contentUpload, setContentUpload] = useState({
-		title: "",
-		subTitle: "",
-		image: null,
-	});
-	const { uploadArtikel } = ArticleZustand();
+  const [content, setContent] = useState("");
+  const [contentUpload, setContentUpload] = useState({
+    title: "",
+    subTitle: "",
+    image: null, 
+  });
+  const { uploadArtikel } = ArticleZustand();
 
-	const handleTyping = (e) => {
-		const { name, value } = e.target;
-		setContentUpload({
-			...contentUpload,
-			[name]: value,
-		});
-	};
+  const handleTyping = (e) => {
+    const { name, value } = e.target;
+    setContentUpload({
+      ...contentUpload,
+      [name]: value,
+    });
+  };
 
-	const handleSave = () => {
-		console.log("Artikel disimpan:", { ...contentUpload, content });
-		setOpenModal(false);
-		uploadArtikel({ ...contentUpload, content });
-	};
+  const handleSave = () => {
+    console.log("Artikel disimpan:", { ...contentUpload, content });
+    setOpenModal(false);
+    uploadArtikel({ ...contentUpload, content });
+  };
 
-	const handleUploadChange = (info) => {
-		console.log(`info`, info.file.originFileObj);
-		setContentUpload({
-			...contentUpload,
-			image: info.file.originFileObj,
-		});
-	};
+  const handleUploadChange = (info) => {
+    console.log(`info`, info.file.originFileObj);
+      setContentUpload({
+        ...contentUpload,
+        image: info.file.originFileObj,
+      });
+   
+  };
 
-	return (
-		<Modal
-			title="Artikel Baru"
-			open={openModal}
-			onCancel={() => setOpenModal(false)}
-			footer={null}
-			width={900}
-			style={{ height: 700, display: "flex", flexDirection: "column" }}
-		>
-			{typeof window !== "undefined" && (
-				<>
-					<Row className="flex flex-col space-y-3">
-						<Input
-							name="title"
-							value={contentUpload.title}
-							onChange={handleTyping}
-							placeholder="Masukkan Title"
-						/>
-						<Input
-							name="subTitle"
-							value={contentUpload.subTitle}
-							onChange={handleTyping}
-							placeholder="Masukkan Sub Title"
-						/>
-						<Upload onChange={handleUploadChange}>
-							<Button>Masukkan Foto</Button>
-						</Upload>
-					</Row>
-					<CKEditor
-						editor={ClassicEditor}
-						data={content}
-						onChange={(event, editor) => {
-							const data = editor.getData();
-							setContent(data);
-						}}
-						config={{
-							toolbar: [
-								'heading', '|',
-								'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|',
-								'undo', 'redo'
-							]
-						}}
-						style={{ height: "400px", marginBottom: "50px" }}
-					/>
-					<div
-						style={{
-							marginTop: "auto",
-							display: "flex",
-							justifyContent: "flex-end",
-						}}
-					>
-						<Button
-							onClick={() => setOpenModal(false)}
-							style={{ marginRight: 10 }}
-						>
-							Batal
-						</Button>
-						<Button
-							style={{ backgroundColor: "#fddb1d", color: "black" }}
-							onClick={handleSave}
-						>
-							Simpan
-						</Button>
-					</div>
-				</>
-			)}
-		</Modal>
-	);
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  return (
+    <Modal
+      title="Artikel Baru"
+      open={openModal}
+      onCancel={() => setOpenModal(false)}
+      footer={null}
+      width={900}
+      style={{ height: 700, display: "flex", flexDirection: "column" }}
+    >
+      {typeof window !== "undefined" && (
+        <>
+          <Row className="flex flex-col space-y-3">
+            <Input
+              name="title"
+              value={contentUpload.title}
+              onChange={handleTyping}
+              placeholder="Masukkan Title"
+            />
+            <Input
+              name="subTitle"
+              value={contentUpload.subTitle}
+              onChange={handleTyping}
+              placeholder="Masukkan Sub Title"
+            />
+            <Upload onChange={handleUploadChange}>
+              <Button>Masukkan Foto</Button>
+            </Upload>
+          </Row>
+          <ReactQuill
+            theme="snow"
+            className="mt-3"
+            value={content}
+            onChange={setContent}
+            modules={modules}
+            formats={formats}
+            style={{ height: "400px", marginBottom: "50px" }}
+          />
+          <div
+            style={{
+              marginTop: "auto",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              onClick={() => setOpenModal(false)}
+              style={{ marginRight: 10 }}
+            >
+              Batal
+            </Button>
+            <Button
+              style={{ backgroundColor: "#fddb1d", color: "black" }}
+              onClick={handleSave}
+            >
+              Simpan
+            </Button>
+          </div>
+        </>
+      )}
+    </Modal>
+  );
 }
