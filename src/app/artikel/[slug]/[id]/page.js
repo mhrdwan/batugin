2b174e6322c3f1b8a929dbdce17ff607ec60b5ca
Administrator <gitlab_admin_76a543@example.com>
2024-07-13 23:40:18 +0700

    test

--- src/app/artikel/[slug]/[id]/page.js
+++ src/app/artikel/[slug]/[id]/page.js
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import NavbarComp from "@/components/NavbarComp";
 import PointKamu from "@/components/PointKamu/page";
-import bannerartikel from "../../../../../public/assets/bannerartikerl.png";
 import Image from "next/image";
 import ArtikelKami from "@/components/Home/ArtikelKami";
 import FooterComp from "@/components/Home/Footer";
@@ -30,41 +29,39 @@ export default function Artikel({ params }) {
   if (!isClient) {
     return null; // Atau spinner/loading state
   }
-  // console.log(`detailDataArticle.data?.[0]?.foto`,detailDataArticle?.data?.[0]?.foto)
+
+  const articleData = detailDataArticle?.data?.[0];
+
   return (
     <div className="bg-white">
       <Head>
         <title>
-          {detailDataArticle?.sub_title} - {detailDataArticle?.title}
+          {articleData?.sub_title} - {articleData?.title}
         </title>
         <meta
           name="description"
-          content={
-            detailDataArticle?.description || detailDataArticle?.sub_title
-          }
+          content={articleData?.description || articleData?.sub_title}
         />
         <meta
           name="keywords"
           content="batu ginjal, kesehatan, artikel kesehatan"
         />
-        <meta property="og:title" content={detailDataArticle?.title} />
+        <meta property="og:title" content={articleData?.title} />
         <meta
           property="og:description"
-          content={
-            detailDataArticle?.description || detailDataArticle?.sub_title
-          }
+          content={articleData?.description || articleData?.sub_title}
         />
-        <meta property="og:image" content={detailDataArticle?.foto} />
-        <meta property="og:type" content="detailDataArticle" />
+        <meta property="og:image" content={articleData?.foto} />
+        <meta property="og:type" content="article" />
         <meta
           property="og:url"
-          content={`https://batugin.co.id/artikel/${detailDataArticle?.id}`}
+          content={`https://batugin.co.id/artikel/${articleData?.id}`}
         />
       </Head>
       <NavbarComp />
       <div className="relative h-[8.9rem] md:h-[30rem] bg-red-200 overflow-hidden">
         <Image
-          src={detailDataArticle.data?.[0]?.foto}
+          src={articleData?.foto}
           alt="Banner Artikel"
           layout="fill"
           objectFit="cover"
@@ -74,21 +71,16 @@ export default function Artikel({ params }) {
 
       <div className="pl-[1.42rem] pr-[1.42rem] md:pl-[8.5rem] md:pr-[8.5rem]">
         <h1 className="text-[12px] md:text-[24px] font-semibold text-start mt-6 font-poopin">
-          <title>
-            {detailDataArticle?.data?.[0]?.sub_title} -{" "}
-            {detailDataArticle?.data?.[0]?.title}
-          </title>
+          {articleData?.sub_title} - {articleData?.title}
         </h1>
         <p className="text-start md:text-[16px] text-[10px] mt-2 text-gray-600">
-          {detailDataArticle?.data?.[0]?.createdAt}
+          {articleData?.createdAt}
         </p>
         <div className="mt-6  w-full">
-          {detailDataArticle?.data?.[0]?.content && (
+          {articleData?.content && (
             <div
-              dangerouslySetInnerHTML={{
-                __html: detailDataArticle.data[0].content,
-              }}
-              className="prose  "
+              dangerouslySetInnerHTML={{ __html: articleData.content }}
+              className="prose"
               style={styles.content}
             ></div>
           )}
