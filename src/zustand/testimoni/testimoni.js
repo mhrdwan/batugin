d9f81d09bccc97ac5b5bb6547dfeeab8b6d879dc
Administrator <gitlab_admin_76a543@example.com>
2024-08-06 10:16:00 +0700

    ridwan

--- src/zustand/testimoni/testimoni.js
+++ src/zustand/testimoni/testimoni.js
@@ -10,8 +10,20 @@ export const testimoniZustand = create((set, get) => ({
       const data = await axios.get(
         `${baseURL}testimoni/get-testimoni?limit=10&page=1`
       );
-    //   console.log(data);
+      //   console.log(data);
       set({ dataTestimoni: data?.data });
+    } catch (error) {}
+  },
+  addTestimoni: async (data) => {
+    const formData = new FormData();
+    formData.append("cover", data?.cover);
+    formData.append("name", data?.name);
+    formData.append("testimoni", data?.testimoni);
+    formData.append("age", data?.age);
+    formData.append("star", data?.star);
+    formData.append("job", data?.job);
+    try {
+      const data = await axios.post(`${baseURL}testimoni/add-testimoni`);
     } catch (error) {}
   },
 }));
