48471d9fbefb59291e1697c6d916364f31bba20f
Administrator <gitlab_admin_76a543@example.com>
2024-08-24 22:43:01 +0700

    test

--- src/components/Give Away/giveAway.js
+++ src/components/Give Away/giveAway.js
@@ -1,7 +1,8 @@
 import { giveAwayZustandStore } from "@/zustand/GiveAway/giveAway";
+import { Modal, Button } from "antd";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function GiveAway() {
   const { fetchGetListGA, dataFetchGetListGA } = giveAwayZustandStore();
@@ -13,37 +14,82 @@ export default function GiveAway() {
   const truncateText = (text, maxLength) => {
     return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
   };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState({});
 
+  const handleLinkClick = (item) => {
+    setModalContent({
+      imageSrc: item.foto,
+      title: item.title,
+      description: item.content,
+    });
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="p-5">
       {/* <p className="font-semibold mb-2">Give Away</p> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
         {dataFetchGetListGA?.data?.map((item) => (
           <a
-          target="_blank"
-            href={item?.link}
-            key={item.id}
+            type="link"
+            onClick={() => handleLinkClick(item)}
             className="flex bg-white rounded-lg overflow-hidden mb-5 hover:cursor-pointer"
           >
-            <div className="w-1/2 flex justify-center items-center overflow-hidden">
+            <div className="w-1/6 aspect-w-1 aspect-h-1 relative overflow-hidden group">
               <Image
                 src={item.foto}
                 alt={item.title}
-                className="object-cover transform transition-transform duration-300 hover:scale-110"
-                width={180}
-                height={180}
-                layout="intrinsic"
+                className="object-cover w-full h-full transform transition-transform duration-300 group-hover:scale-110"
+                layout="fill"
               />
             </div>
-            <div className="w-1/2 p-2 flex flex-col justify-between">
+            <div className="w-5/6 p-4 flex flex-col justify-between">
               <p className="text-md font-semibold">
                 {truncateText(item.title, 20)}
               </p>
-              <div className="">{truncateText(item.content, 50)}</div>
+              <div className="text-sm text-gray-600">
+                {truncateText(item.content, 50)}
+              </div>
             </div>
           </a>
         ))}
+        <AntdModal
+          isModalOpen={isModalOpen}
+          handleCancel={handleCancel}
+          content={modalContent}
+        />
       </div>
     </div>
   );
 }
+
+const AntdModal = ({ isModalOpen, handleCancel, content }) => (
+  <Modal
+    title={content.title}
+    open={isModalOpen}
+    onCancel={handleCancel}
+    footer={null}
+    width={800}
+    bodyStyle={{ height: "400px" }} // Set height to a fixed value
+  >
+    <div className="flex h-full">
+      <div className="w-1/2 p-2 h-full">
+        <img
+          src={content.imageSrc}
+          alt={content.title}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+      <div className="w-1/2 p-2 h-full flex flex-col ">
+      <p className="font-bold">{content.title}</p>
+        <p className="text-sm text-gray-600 overflow-auto">
+          {content.description}
+        </p>
+      </div>
+    </div>
+  </Modal>
+);
