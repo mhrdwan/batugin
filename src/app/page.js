5d7221d424c27eed101c294dced158cafd63cc2f
Administrator <gitlab_admin_76a543@example.com>
2024-08-24 18:26:41 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,13 @@ export const metadata = {
     title: "Batugin",
     description: "Obat Peluruh Batu Ginjal PERTAMA di Indonesia",
     url: "https://www.batugin.co.id/",
-    siteName: "Admin Batugin",
+    siteName: "Batugin - Obat Peluruh Batu Ginjal PERTAMA di Indonesia",
     images: [
       {
-        url: "https://batugin.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBANNER%20BATUGIN%20-%201440%20x%20530-03.8c430ccc.png&w=3840&q=75",
+        url: JSON.stringify(banner1),
         width: 800,
         height: 600,
-        alt: "Admin Batugin",
+        alt: "Batugin - Obat Peluruh Batu Ginjal PERTAMA di Indonesia",
       },
     ],
     icon: "./icon.png",
